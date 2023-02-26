edf022fff2f0d58283c3206714f6152bf58a5212
Kedar kishan j <kedarkishanj14@gmail.com>
2023-02-26 22:37:16 +0530

    everything updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import'./App.css';
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Home from './components/Home'
+import { Navbar } from './components/Navbar'
+import { About } from "./components/About";
+import { Signup } from './components/Signup';
+import { Contact } from "./components/Contact";
+import { Login } from "./components/Login";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+
+    <Navbar/>
+    <Routes>
+    <Route exact path='/' element={<Home/>}></Route>
+    <Route path='/About' element={<About/>}></Route>
+    <Route path='/Contact' element={<Contact/>}></Route>
+    <Route path='/Login' element={<Login/>}></Route>
+    <Route path='/Signup' element={<Signup/>}></Route> 
+    </Routes>
+    </>
+   
+  )
 }
 
-export default App;
+export default App
