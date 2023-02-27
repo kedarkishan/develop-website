e0af47fa4086d9d48e2f1d2a927cbddaee706cee
Kedar kishan j <kedarkishanj14@gmail.com>
2023-02-27 23:59:00 +0530

    contact us changed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,32 +3,32 @@ import React from 'react'
 export const Contact = () => {
   return (
     <div className='container5'> 
-     <div className="col1for3 gridItemCard-module_root_ocWhg" style={{background: "#fb3d674d"}}>
-         <img src="https://academy98.com/wp-content/themes/academy/images/icon/contact-icon1.png" alt="loading" className="tie-appear"/>
-         <h2 className="fatitle">Your Title</h2>
-         <span className="entitle">info@gmail.com</span>
-         
-          <p></p>
-</div>
-
-<div className="col1for3 gridItemCard-module_root_ocWhg" style={{background: "#fb3d674d"}}>
-                            <img src="https://static.vecteezy.com/system/resources/thumbnails/002/323/648/small/phone-icon-flat-style-isolated-on-white-background-free-vector.jpg"alt='loading' className="tie-appear"/>
-                            <h2 className="fatitle">Call Us</h2>
-                            <span className="entitle">+91 234567676</span>
-                            
-                          <p></p>
-                        </div>
-                        <div className="col1for3 gridItemCard-module_root_ocWhg" style={{background: "#fb3d674d"}}>
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFOV99Giaw_GBBx-7SxEbv0qdk_N59MMGP_Q&usqp=CAU" alt='loading'/>
-                            <h2 className="fatitle">Address</h2>
-                            <span className="entitle">mangaluru</span>
-                            
-                           <p></p>
-                        </div>
-
-<br />
-<br />
-
+      
+      <h1 className="title">CONTACT US</h1>
+    <div className="container8">
+      <div className="container__elem">
+        <div className="elem__block">
+          <img className="hover-img" src="https://i.postimg.cc/XvK00tKD/call.png" alt='loading' width="50%" />
+        </div>
+        <h2 className="text">CALL US</h2>
+        <p className="text">Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="container__elem">
+        <div className="elem__block">
+          <img className="hover-img" src="https://i.postimg.cc/Fzzt3RM2/write.png" alt='loading' width="50%" />
+        </div>
+        <h2 className="text">WRITE US</h2>
+        <p className="text">Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="container__elem">
+        <div className="elem__block">
+          <img className="hover-img" src="https://static.thenounproject.com/png/1542433-200.png" alt="loading" width="50%" />
+        </div>
+        <h2 className="text">Location</h2>
+        <p className="text">Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+    
     </div>
   )
 }
